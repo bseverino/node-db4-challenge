1c15e9ec498f2c3c0e7cdbcd8674aa9674858877
B Severino <biancasev@gmail.com>
2020-01-23 13:22:05 -0500

    Set up boilerplate

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+
+}
