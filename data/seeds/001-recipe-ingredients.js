d87027b9fb92bf494c95ffe03ed0f7ff2951038a
B Severino <biancasev@gmail.com>
2020-01-23 16:59:49 -0500

    Created seeds

---
+++ data/seeds/001-recipe-ingredients.js
@@ -0,0 +1,93 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipe-ingredients').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipe-ingredients').insert([
+        {
+          id: 1,
+          recipe_id: 1,
+          ingredient_id: 1,
+          ingredient_count: 400
+        },
+        {
+          id: 2,
+          recipe_id: 1,
+          ingredient_id: 2,
+          ingredient_count: 3
+        },
+        {
+          id: 3,
+          recipe_id: 1,
+          ingredient_id: 3,
+          ingredient_count: 3
+        },
+        {
+          id: 4,
+          recipe_id: 1,
+          ingredient_id: 4,
+          ingredient_count: 2
+        },
+        {
+          id: 5,
+          recipe_id: 1,
+          ingredient_id: 10,
+          ingredient_count: 0.5
+        },
+        {
+          id: 6,
+          recipe_id: 1,
+          ingredient_id: 11,
+          ingredient_count: 0.5
+        },
+        {
+          id: 7,
+          recipe_id: 2,
+          ingredient_id: 5,
+          ingredient_count: 200
+        },
+        {
+          id: 8,
+          recipe_id: 2,
+          ingredient_id: 2,
+          ingredient_count: 2
+        },
+        {
+          id: 9,
+          recipe_id: 2,
+          ingredient_id: 6,
+          ingredient_count: 0.25
+        },
+        {
+          id: 10,
+          recipe_id: 2,
+          ingredient_id: 7,
+          ingredient_count: 1.5
+        },
+        {
+          id: 11,
+          recipe_id: 2,
+          ingredient_id: 8,
+          ingredient_count: 0.5
+        },
+        {
+          id: 12,
+          recipe_id: 2,
+          ingredient_id: 8,
+          ingredient_count: 0.5
+        },
+        {
+          id: 13,
+          recipe_id: 2,
+          ingredient_id: 9,
+          ingredient_count: 0.5
+        },
+        {
+          id: 14,
+          recipe_id: 2,
+          ingredient_id: 10,
+          ingredient_count: 1
+        }
+      ]);
+    });
+};
